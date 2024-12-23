ad7eff98380ab3989874d99eed3a8410fd24a8e9
alina <alinaaziz831@gmail.com>
2024-12-24 01:47:26 +0500

    first commit

---
+++ src/components/about.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat vitae vel atque ipsa, nam officiis odit! Officia perferendis architecto debitis animi accusantium, odio neque cum illum culpa tempore similique, est rerum quod.
+    </div>
+  )
+}
+
+export default About
